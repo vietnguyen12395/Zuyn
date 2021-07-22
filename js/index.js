1d3c00e0166ab9b9996f2fe963658ccb4190158c
vietnguyen12395 <50574839+vietnguyen12395@users.noreply.github.com>
2021-07-22 20:14:30 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -116,7 +116,7 @@ var my_gradient = ctx1.createLinearGradient(0, 0, 0, 170);
 my_gradient.addColorStop(0, "gray");
 my_gradient.addColorStop(1, "white");
 ctx1.fillStyle = my_gradient;
-const text = 'Emmy 💙';
+const text = 'Ngọc Anh Hâm';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
